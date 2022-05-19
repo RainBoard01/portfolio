44217dc13d41eea03c36ed51ee191a13a2eab9ca
rainboard01 <amdmsi4ever@hotmail.com>
2022-05-19 18:14:09 -0400

    Fix: missing types interface on section component

--- components/Section/section.tsx
+++ components/Section/section.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { motion } from 'framer-motion';
 
-export const Section = ({ children, delay = 0 }: { children: any; delay: number }): JSX.Element => (
+interface Props {
+  children: any;
+  delay: number;
+}
+
+export const Section: FC<Props> = ({ children, delay = 0 }): JSX.Element => (
   <motion.div
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
