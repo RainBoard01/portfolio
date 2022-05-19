ad2a19fa903e87ebf91720e37238f7418c86bbcf
rainboard01 <amdmsi4ever@hotmail.com>
2022-05-19 17:59:46 -0400

    Fix: children type error

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -1,4 +1,6 @@
 import { Container } from '@mantine/core';
 import React from 'react';
 
-export const Layout = ({ children }) => <Container size="xs">voxel{children}footer</Container>;
+export const Layout = ({ children }: { children: any }) => (
+  <Container size="xs">voxel{children}footer</Container>
+);
