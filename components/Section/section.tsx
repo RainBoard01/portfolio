72a224142fe276fb6cab0159121e88a69f98850c
rainboard01 <amdmsi4ever@hotmail.com>
2022-05-19 18:02:34 -0400

    Fix: another children type error

--- components/Section/section.tsx
+++ components/Section/section.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-export const Section = ({ children, delay = 0 }): JSX.Element => (
+export const Section = ({ children, delay = 0 }: { children: any; delay: number }): JSX.Element => (
   <motion.div
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
